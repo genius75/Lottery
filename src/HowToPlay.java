779005b769c6b12dfaafb5ad1ad5bf21b7df82cb
陳弘哲 <ronald75@gmail.com>
2015-07-24 23:19:10 +0800

    發佈v1.0
    修改程式標題與新增發佈日期於"說明"

--- src/HowToPlay.java
+++ src/HowToPlay.java
@@ -12,7 +12,7 @@ import javax.swing.JPanel;
 
 public class HowToPlay extends JFrame {
 	private JPanel pan;
-	private JLabel SL638,L649,L740,DC,tD,fD,fb,google;
+	private JLabel SL638,L649,L740,DC,tD,fD,fb,google,date;
 
 	public HowToPlay() {
 		this.setTitle("台灣彩卷-玩法說明");
@@ -67,6 +67,10 @@ public class HowToPlay extends JFrame {
 		google.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		pan.add(google);
 		goWebsite(google);
+		
+		date=new JLabel();
+		date.setText("　　發佈日期：2015.7.24");
+		pan.add(date);
 		
 		this.setContentPane(pan);
 		this.setVisible(true);
